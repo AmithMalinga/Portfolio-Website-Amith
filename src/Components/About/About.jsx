a0f2f654aec5387946e6c8b7f9d94e2be81e691a
K Amith Malinga <malingaamith1@gmail.com>
2024-08-28 19:26:12 +0530

    Initial commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/profile_img.svg'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className="about-title">
+                <h1>About me</h1>
+                <img src={theme_pattern} alt='' />
+            </div>
+            <div className="about-sections">
+                <div className="about-left">
+                    <img src={profile_img} alt='' />
+                </div>
+                <div className="about-right">
+                    <div className="about-para">
+                        <p> uhdd wiuwe fwef fewhffao; g wafrig r r;gjrigjserligg ri;ogrig rugherg rgshgre;oig rohgr</p>
+                        <p>euefgwe wye rywerwewey weyfuwa wewehwew whiewu ieri aroiuaepor;oirthghithee nu  iitwiojwoi wo awio weoi go wg gho aghr g ;agr i;ho ra</p>
+                    </div>
+                    <div className="about-skills">
+                        <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "70%" }} /></div>
+                        <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "60%" }} /></div>
+                        <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "50%" }} /></div>
+                        <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "50%" }} /></div>
+                    </div>
+                </div>
+            </div>
+            <div className="about-achievements">
+                <div className='about-achievement'>
+                    <h1>10+</h1>
+                    <p>YEARS OF EXPERIRENCE</p>
+                </div>
+                <hr/>
+                <div className='about-achievement'>
+                    <h1>90+</h1>
+                    <p>PROJECTS COMPLETED</p>
+                </div> 
+                <hr/>
+                <div className='about-achievement'>
+                    <h1>90+</h1>
+                    <p>PROJECTS COMPLETED</p>
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React, { Profiler } from 'react'
+import './Hero.css'
+import Profiler_img from '../../assets/profile_img.svg'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={Profiler_img} alt="" />
+      <h1><span>I'm Amith Malinga,</span> Software Developer Based on Sri Lanka.</h1>
+      <p>I am a dedicated software engineer with a strong passion for developing innovative solutions.</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
