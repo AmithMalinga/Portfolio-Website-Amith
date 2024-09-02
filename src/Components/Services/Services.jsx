f65f7c6a1851e5f6a99c5da916cb1db91d2d0398
K Amith Malinga <malingaamith1@gmail.com>
2024-09-02 10:49:12 +0530

    Services section created

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Services.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import Services_Data from '../../assets/services_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
+
+const Services = () => {
+    return (
+        <div className='services'>
+            <div className="services-title">
+                <h1>My services</h1>
+                <img src={theme_pattern} alt="Theme Pattern" />
+            </div>
+            <div className="service-container">
+                {Services_Data.map((Service, index) => {
+                    return (
+                        <div key={index} className="service-format">
+                            <h3>{Service.s_no}</h3>
+                            <h2>{Service.s_name}</h2>
+                            <p>{Service.s_desc}</p>
+                            <div className="services-readmore">
+                                <p>Read More</p>
+                                <img src={arrow_icon} alt="Arrow Icon" />
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Services;
